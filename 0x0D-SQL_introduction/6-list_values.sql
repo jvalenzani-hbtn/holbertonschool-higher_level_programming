11c38be4f91c8d547a897bd6a50b12c53cad843b
Javier Valenzani <javier.valenzani@holbertonschool.com>
2020-12-07 13:27:13 +0000

    update

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Lilst all values in table first_table
+SELECT * FROM `first_table`;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Insert value in first_table
+INSERT INTO `first_table` (`id`, `name`) VALUES ('89',  'Holberton School');
