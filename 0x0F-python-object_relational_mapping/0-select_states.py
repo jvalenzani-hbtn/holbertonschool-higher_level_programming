4268c43f372406ee53d738a726d9c27900e270f6
Javier Valenzani <javier.valenzani@holbertonschool.com>
2021-03-25 16:47:35 +0000

    Fix doc

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
+"""Aca haciendo una consulta a la BD con la barra."""
 
-from sys import argv
-import MySQLdb
+if __name__ == 'main':
+  from sys import argv
+  import MySQLdb
 
-
-MY_HOST = "localhost"
-MY_USER = argv[1]
-MY_PASS = argv[2]
-MY_DB = argv[3]
-# Hago la conexion a la BD. Obtengo un objeto db
-try:
-  db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-  # Obtengo un cursor para ejecutar las consultas
-  cur = db.cursor()
-  # Ejecuto consulta usando el cursor
-  cur.execute("SELECT * FROM states ORDER BY id")
-  # Obtengo todas las filas
-  rows = cur.fetchall()
-  # Recorro el resultado
-  for r in rows:
-    print(r)
-except Exception as e:
-  print("ERROR: {}".format(e))
+  MY_HOST = "localhost"
+  MY_USER = argv[1]
+  MY_PASS = argv[2]
+  MY_DB = argv[3]
+  # Hago la conexion a la BD. Obtengo un objeto db
+  try:
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    # Obtengo un cursor para ejecutar las consultas
+    cur = db.cursor()
+    # Ejecuto consulta usando el cursor
+    cur.execute("SELECT * FROM states ORDER BY id")
+    # Obtengo todas las filas
+    rows = cur.fetchall()
+    # Recorro el resultado
+    for r in rows:
+      print(r)
+  except Exception as e:
+    print("ERROR: {}".format(e))
