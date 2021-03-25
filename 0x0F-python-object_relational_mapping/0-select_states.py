743a3fdf6904bcc11b93668bd2e25dfaea3d5e47
Javier Valenzani <javier.valenzani@holbertonschool.com>
2021-03-25 17:12:16 +0000

    cuestiones

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,20 @@
 Aca haciendo una consulta a la BD con la barra.
 '''
 
-from sys import argv
-import MySQLdb
-
-if __name__ == 'main':
-  MY_HOST = "localhost"
-  MY_USER = argv[1]
-  MY_PASS = argv[2]
-  MY_DB = argv[3]
-  try:
-      db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-      cur = db.cursor()
-      cur.execute("SELECT * FROM states ORDER BY id")
-      rows = cur.fetchall()
-      for r in rows:
-          print(r)
-      db.close()
-  except Exception as e:
-      print("ERROR: {}".format(e))
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    MY_HOST = "localhost"
+    MY_USER = argv[1]
+    MY_PASS = argv[2]
+    MY_DB = argv[3]
+    try:
+        db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id")
+        rows = cur.fetchall()
+        for r in rows:
+            print(r)
+        db.close()
+    except Exception as e:
+        print("ERROR: {}".format(e))
