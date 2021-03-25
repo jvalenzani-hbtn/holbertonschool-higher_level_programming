4fe432481d9c935d8f29b405b93a29c5781157fc
Javier Valenzani <javier.valenzani@holbertonschool.com>
2021-03-25 17:00:45 +0000

    fuuuuu

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,19 +11,19 @@ MY_PASS = argv[2]
 MY_DB = argv[3]
 # Hago la conexion a la BD. Obtengo un objeto db
 try:
-  db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
-  # Obtengo un cursor para ejecutar las consultas
-  cur = db.cursor()
-  # Ejecuto consulta usando el cursor
-  cur.execute("SELECT * FROM states ORDER BY id")
-  # Obtengo todas las filas
-  rows = cur.fetchall()
-  # Recorro el resultado
-  for r in rows:
-    print(r)
-  db.close()
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    # Obtengo un cursor para ejecutar las consultas
+    cur = db.cursor()
+    # Ejecuto consulta usando el cursor
+    cur.execute("SELECT * FROM states ORDER BY id")
+    # Obtengo todas las filas
+    rows = cur.fetchall()
+    # Recorro el resultado
+    for r in rows:
+        print(r)
+    db.close()
 except Exception as e:
-  print("ERROR: {}".format(e))
+    print("ERROR: {}".format(e))
 
 if __name__ == 'main':
-  pass
+    pass
