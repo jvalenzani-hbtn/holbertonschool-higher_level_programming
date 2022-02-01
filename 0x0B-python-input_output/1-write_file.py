292308c7e7a2be75033c9d4eb38971f095373621
Javier Valenzani <javier.valenzani@holbertonschool.com>
2022-02-01 17:28:00 -0300

    0x0B 1 and 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Write to a file"""
 
 def write_file(filename="", text=""):
+    """Write to a file"""
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Append to a file"""
+
+def append_write(filename="", text=""):
+    """Append to a file"""
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
