d57d161f3c6d605279cf037ac6a494ce497931b6
Javier Valenzani <javier.valenzani@holbertonschool.com>
2022-02-01 18:21:44 -0300

    0x0B 10 - empty list fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,12 +10,10 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
+        if(attrs == None):
+            return(self.__dict__)
         new_dict = {}
-        if(len(attrs) > 0):
-            if(not attrs):
-                new_dict = self.__dict__
-            else:
-                for k,v in self.__dict__.items():
-                    if (k in attrs):
-                        new_dict[k] = v
+        for k,v in self.__dict__.items():
+            if (k in attrs):
+                new_dict[k] = v
         return new_dict
