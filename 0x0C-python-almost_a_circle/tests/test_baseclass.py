43810ef22f8df03413809f200ddb0dabf628f992
Javier Valenzani <javier.valenzani@holbertonschool.com>
2022-02-03 16:22:45 -0300

    0x0C - Base + Test

---
+++ 0x0C-python-almost_a_circle/tests/test_baseclass.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from models.base import Base
+import unittest
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_instance_autonumber(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b2.id - 1, b1.id)
+
+    def test_new_instance(self):
+        b = Base()
+        self.assertTrue(isinstance(b, Base))
+
+    def test_new_instance_id(self):
+        id = 21
+        b = Base(id)
+        self.assertEqual(b.id, id)
+    
+    def test_not_num_id(self):
+        # id = "NaN"
+        # b = Base(id)
+        # self.?
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
